5fec7fd51c33cf83866435aaf7a13fa2d935ccb7
Joshua Wallace <jtrwallcae@mac.com>
2014-03-29 09:25:04 -0700

    Completed Computer.java
    Wrote all the methods for Computer class. Obviously if we have more to
    add its not complete but it completes our preliminary UML.

--- Computer.java
+++ Computer.java
@@ -11,22 +11,27 @@ public class Computer extends Player {
     
     /*Default constructor calls generateResponseTime() & generateNumberOfCards() & increments totalCards.*/
     public Computer() {
-        
+        generateResponseTime();
+        generateNumberOfCards();
+        Card.totalCards++;
     }
     
     /*Randomly generates # of cards between 1 and 4.*/
-    public void generateNumberOfCards() {
-        
+    public final void generateNumberOfCards() {
+        int numberOfCards = (int)((Math.random() * (4 - 1)) + 1);
+        for (int i = 0; i < numberOfCards; i++) {
+            playerCards.add(new Card());
+        }
     }
     
     /*Randomly generates # of seconds between 0 and 3.*/
-    public void generateResponseTime() {
-        
+    public final void generateResponseTime() {
+        responseTime = ((Math.random() * (4 - 1)) + 1);
     }
     
     /*Set the response time.*/
     public void setResponseTime(double responseTime) {
-        
+        this.responseTime = responseTime;
     }
     
     /*Returns the response time.*/
