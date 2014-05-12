1393022a012fbf7d3f2027b54949fdb569e67886
gregknight <gregknight@gmail.com>
2014-05-11 17:10:13 -0700

    StatusWindow now shows the number of ComputerCards that are in play. Changed a few things in Computer. Wrote ComputerCard as an inner class of Computer, but it's not finished (it's still little more than a stub).

--- Computer.java
+++ Computer.java
@@ -7,25 +7,34 @@
 import java.util.Random;
 
 public class Computer {
-    static int NumComputerPlayers = 0;
+    static int totalComputerPlayers = 0;
+    static int totalComputerCards = 0;
     
     /*Instance variables*/
     private double responseTime;
+    private final ComputerCard[] cards;
     
     /*Default constructor calls generateResponseTime() & generateNumberOfCards() & increments totalCards.*/
     public Computer() {
         generateResponseTime();
-        generateNumberOfCards();
-	NumComputerPlayers++;
+        cards = generateComputerCards();
+	totalComputerPlayers++;
     }
     
     /*Randomly generates # of cards between 1 and 4.*/
-    public final void generateNumberOfCards() {
-        int numberOfCards = new Random().nextInt(4) + 1;   
+    private ComputerCard[] generateComputerCards() {
+	int numberOfCards = new Random().nextInt(4) + 1;
+	ComputerCard[] temp = new ComputerCard[numberOfCards];
+	
+	for (int i = 0; i < numberOfCards; i++) {
+	    temp[i] = new ComputerCard();
+	}
+	
+	return temp;
     }
     
     /*Randomly generates # of seconds between 0 and 3.*/
-    public final void generateResponseTime() {
+    private void generateResponseTime() {
         responseTime = ((Math.random() * (4 - 1)) + 1);
     }
     
@@ -37,5 +46,34 @@ public class Computer {
     /*Returns the response time.*/
     public double getResponseTime() {
         return responseTime;
-    } 
+    }
+    
+    public void receiveNewNumber(int n) {
+	for (ComputerCard card : cards) {
+	    if (!card.isBingo()) {
+		card.markAndCall(n);
+	    }
+	}
+    }
+    
+    private class ComputerCard {
+	private boolean isBingo = false;
+	// Something that stores the numbers
+	// Something that stores whether a given number is marked or not
+	
+	private ComputerCard() {
+	    totalComputerCards++;
+	    // Generate random numbers
+	}
+	
+	public boolean isBingo() {
+	    return isBingo;
+	}
+	
+	public void markAndCall(int n) {
+	// For efficiency, search only the column that "n" could possibly be in, and mark if found.
+	// Then, search the entire card for the any valid bingo pattern.
+	// If bingo, set isBingo to true and decrement the number of bingos available.
+	}
+    }
 }
--- StatusWindow.java
+++ StatusWindow.java
@@ -10,9 +10,7 @@ import javax.swing.JLabel;
 
 public class StatusWindow extends JLabel {
     private final Bingo b;
-    private final ImageIcon background = new ImageIcon(getClass().getResource("/img/StatusWindow/GameplayTracker.png"));
-    private static final Font bingosLeftFont = BingoGUI.getGameFont().deriveFont(35f);
-    private static final Font smallerFont = BingoGUI.getGameFont().deriveFont(25f);
+    private final Font smallerFont = BingoGUI.getGameFont().deriveFont(25f);
     private final JLabel bingosLeft = new JLabel();
     private final JLabel compOpponents = new JLabel();
     private final JLabel cardsInPlay = new JLabel();
@@ -20,11 +18,11 @@ public class StatusWindow extends JLabel {
     public StatusWindow(Bingo b) {
 	this.b = b;
 	
-	this.setIcon(background);
+	this.setIcon(new ImageIcon(getClass().getResource("/img/StatusWindow/GameplayTracker.png")));
 	
 	bingosLeft.setSize(75, 45);
         bingosLeft.setLocation(155, 20);
-        bingosLeft.setFont(bingosLeftFont);
+        bingosLeft.setFont(BingoGUI.getGameFont().deriveFont(35f));
 	bingosLeft.setText(String.valueOf(b.getNumberOfBingos()));
 	bingosLeft.setHorizontalAlignment(JLabel.CENTER);
 	this.add(bingosLeft);
@@ -32,14 +30,14 @@ public class StatusWindow extends JLabel {
 	compOpponents.setSize(50, 25);
         compOpponents.setLocation(65, 80);
         compOpponents.setFont(smallerFont);
-	compOpponents.setText(String.valueOf(Computer.NumComputerPlayers));
+	compOpponents.setText(String.valueOf(Computer.totalComputerPlayers));
 	compOpponents.setHorizontalAlignment(JLabel.CENTER);
 	this.add(compOpponents);
 	
 	cardsInPlay.setSize(50, 25);
         cardsInPlay.setLocation(185, 80);
         cardsInPlay.setFont(smallerFont);
-	cardsInPlay.setText("999");
+	cardsInPlay.setText(String.valueOf(Computer.totalComputerCards));
 	cardsInPlay.setHorizontalAlignment(JLabel.CENTER);
 	this.add(cardsInPlay);
     }
