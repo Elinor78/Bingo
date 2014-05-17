140cbb81d9fa35c089a7093a8f8a751a674cbd5f
gregknight <gregknight@gmail.com>
2014-05-17 11:24:59 -0700

    New bingo notifications are now interruptable.

--- NewBingoNotification.java
+++ NewBingoNotification.java
@@ -7,31 +7,26 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
 public class NewBingoNotification extends JLabel {
-    private final TestListener testListener = new TestListener();
-    
     private int alphaValue = 255;
-    private Color textColor = new Color(130, 150, 160, alphaValue);
+    private Color textColor = new Color(0, 0, 0, alphaValue);
     
     private final ActionListener decreaseOpacity = new ActionListener() {
 	@Override
 	public void actionPerformed(ActionEvent evt) {
-	    textColor = new Color(130, 150, 160, alphaValue--);
+	    textColor = new Color(0, 0, 0, alphaValue--);
 	    setBingoText();
 	    
 	    if (alphaValue == 0) {
 		textTimer.stop();
-		alphaValue = 255;
 	    }
 	}
      };
-    private final Timer textTimer = new Timer(5, decreaseOpacity);
+    private final Timer textTimer = new Timer(10, decreaseOpacity);
     
     public NewBingoNotification() {
 	this.setFont(BingoGUI.getGameFont().deriveFont(17f));
@@ -39,24 +34,17 @@ public class NewBingoNotification extends JLabel {
 	this.setHorizontalAlignment(JLabel.CENTER);
 	this.setText("");
 	this.setIcon(new ImageIcon(getClass().getResource("/img/StatusWindow/NewBingoBubble.png")));
-	
-	this.addMouseListener(testListener);
     }
     
     public void showClaimedBingo() {
-	textColor = new Color(130, 150, 160, 255);
+	textTimer.stop(); 
+	
+	alphaValue = 255;
 	textTimer.start();
     }
     
     private void setBingoText() {
 	this.setForeground(textColor);
 	this.setText("An opponent got a bingo!");
-    }
-    
-       private class TestListener extends MouseAdapter {
-	@Override
-	public void mousePressed(MouseEvent e) {
-	    showClaimedBingo();
-	}
     }
 }
