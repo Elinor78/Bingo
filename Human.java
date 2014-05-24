577411996cbbfd74dd7c3a1abe8bbe64431f7b79
Joshua Wallace <jtrwallace@mac.com>
2014-05-24 00:57:34 -0700

    Implemented Multiple Players
    Extensive commit. Mostly completed except need new buttons for New
    Player and possibly some other buttons. I’m just reusing some we
    already have temporarily. This is all managed through a properties file
    which has a lot of error checking to make sure it actually exists and
    if it doesn’t to create it and set ticket values correctly. I will add
    comments in a later commit.

--- Human.java
+++ Human.java
@@ -1,44 +1,34 @@
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Properties;
-
 /*
  * CS 56 Team #1 - Bingo
  * Authors: Joshua Wallace, Sidney Eubanks, Greg Knight, 
  * Elinor Huntington, Linus Carlsson, Armand Flores
  */
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Human {
     private int numberOfCards = 0;
     private final ArrayList<Integer> bankHistory= new ArrayList<>();
     private int ticketsWonInLatestRound = 0;
-    final String name = null;
+    private Set<String> playerNames;
+    private String name = null;
+    //public ChoosePlayer choosePlayer;
+    private Exception Exception;
     
     public Human() {
-	/*If a properties file exists in the user's home directory, use it. If not, just start off with 20 tickets.*/
-	Properties ticketProperties = new Properties();
-	InputStream ticketInputStream = null;
-	try {
-	    ticketInputStream = new FileInputStream(new File(System.getProperty("user.home") + "/bingo.properties"));
-	    ticketProperties.load(ticketInputStream);
-	    bankHistory.add(Integer.parseInt(ticketProperties.getProperty("Tickets")));
-	} catch (IOException | NumberFormatException e) {
-	    System.out.println("Couldn't load the file.");
-	    bankHistory.add(20);
-	}
-	finally {
-	    try {
-		if (ticketInputStream != null) {
-		   ticketInputStream.close(); 
-		}
-	    } catch (IOException ex) {
-		System.out.println("Couldn't close the properties input stream.");
-	    }
-	}
+	
+	initiatePlayer();
     }
     
     /*Change value of ticketBank in positive or negative values.*/
@@ -67,7 +57,7 @@ public class Human {
     }
     
     public int getNumberOfCards() {
-	return numberOfCards;
+	return this.numberOfCards;
     }
     
     public void addTicketsWonInLatestRound(int t) {
@@ -80,6 +70,86 @@ public class Human {
     
     public int getTicketsWonInLatestRound() {
 	return ticketsWonInLatestRound;
+    }
+    
+    public void setName(String n) {
+	this.name = n;
+    }
+    
+    public String getName() {
+	return name;
+    }
+    
+    public Set<String> getPlayerNames() {
+	return playerNames;
+    }
+
+    private void initiatePlayer() {
+	
+	/*If a properties file exists in the user's home directory, use it. If not, just start off with 20 tickets.*/
+	File propertiesFile = new File(System.getProperty("user.home") + "/bingo.properties");
+	Properties ticketProperties = new Properties();
+	InputStream ticketInputStream = null;
+	OutputStream ticketOutputStream = null;
+	
+	try {
+	    ticketInputStream = new FileInputStream(propertiesFile);
+	    ticketProperties.load(ticketInputStream);
+
+	    if (ticketProperties.isEmpty()) {
+		throw Exception;
+	    }
+	    else if (ticketProperties.size() == 1) {
+		playerNames = ticketProperties.stringPropertyNames();
+		for (String tempName : playerNames) {
+		    if (tempName.equals("Default")) {
+			new NewPlayer(this);
+			bankHistory.add(20);
+		    }
+		    else {
+			name = tempName;
+			bankHistory.add(Integer.parseInt(ticketProperties.getProperty(tempName)));
+		    }
+		}
+	    }
+	    else if (ticketProperties.size() > 1) {
+		playerNames = ticketProperties.stringPropertyNames();
+		new ChoosePlayer(this);
+		if (ticketProperties.getProperty(name) == null) {
+		    bankHistory.add(20);
+		}
+		else {
+		    bankHistory.add(Integer.parseInt(ticketProperties.getProperty(name)));
+		}
+	    }
+	} catch (Exception e) {
+	    try {
+		ticketOutputStream = new FileOutputStream(propertiesFile);
+		ticketProperties.setProperty("Default", String.valueOf(20));
+		ticketProperties.store(ticketOutputStream, null);
+		ticketInputStream = new FileInputStream(propertiesFile);
+		ticketProperties.load(ticketInputStream);
+		playerNames = ticketProperties.stringPropertyNames();
+		new NewPlayer(this);
+		bankHistory.add(20);
+	    } catch (FileNotFoundException ex) {
+		Logger.getLogger(Human.class.getName()).log(Level.SEVERE, null, ex);
+	    } catch (IOException ex) {
+		Logger.getLogger(Human.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	}
+	finally {
+	    try {
+		if (ticketInputStream != null) {
+		   ticketInputStream.close(); 
+		}
+		if (ticketOutputStream != null) {
+		    ticketOutputStream.close();
+		}
+	    } catch (IOException ex) {
+		System.out.println("Couldn't close the properties input and/or output stream.");
+	    }
+	}
     }
    
 }
